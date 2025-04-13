b8d4e8e84e113896f8ca9df80d9ac1e42c2c44ce
Ntokozokate <mangenakate@gmail.com>
2025-04-13 09:11:31 +0200

    Removed broken submodule

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -15,6 +15,7 @@ const MovieCard = ({ movie }) => {
           </button>
         </div>
       </div>
+      =
       <div className="movie-info">
         <h3 className="">{movie.title}</h3>
         <p>{movie.release_date}</p>
